e5dd51fa076994a17e83d5e00632a61505fa5361
Tannu Choudhary <142585725+tannuiscoding@users.noreply.github.com>
2024-11-27 15:47:04 +0530

    Create LeftRotateArray.cpp

---
+++ LeftRotateArray.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    void rotateArr(vector<int>& arr, int d) {
+        int n=arr.size();
+        d%=n;
+        int start = 0, end = d - 1;
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        start = d, end = n - 1;
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        start=0, end=n-1;
+        while(start<end){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+};
