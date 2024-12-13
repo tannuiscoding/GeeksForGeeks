dab3084e2cac3004f81d6525a8cb1d3245fe1139
Tannu Choudhary <142585725+tannuiscoding@users.noreply.github.com>
2024-12-13 20:56:11 +0530

    Create day20.cpp

---
+++ gfg160/day20.cpp
@@ -0,0 +1,12 @@
+//check if string is rotated or not
+
+class Solution {
+  public:
+    bool areRotations(string &s1, string &s2) {
+        if (s1.length() != s2.length()) {
+            return false; 
+        }
+        string temp = s1 + s1;
+        return (temp.find(s2) != string::npos);
+    }
+};
