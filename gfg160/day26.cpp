83f735dcca6cabe60c9ac600c156a15a34bba48c
Tannu Choudhary <142585725+tannuiscoding@users.noreply.github.com>
2024-12-19 19:32:05 +0530

    Create day26.cpp

---
+++ gfg160/day26.cpp
@@ -0,0 +1,23 @@
+//non-overlapping intervals
+
+class Solution {
+public:
+    int minRemoval(vector<vector<int>> &intervals) {
+        if (intervals.empty()) return 0;
+        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[1] < b[1];
+        });
+
+        int count = 0;
+        int prevEnd = intervals[0][1];  
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals[i][0] < prevEnd) {
+                count++;
+            } else {
+                prevEnd = intervals[i][1];
+            }
+        }
+
+        return count;
+    }
+};
