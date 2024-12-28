989a8ef67f61c46520331008e8672130634c8e6a
Tannu Choudhary <142585725+tannuiscoding@users.noreply.github.com>
2024-12-28 19:38:31 +0530

    Create day35.cpp

---
+++ gfg160/day35.cpp
@@ -0,0 +1,19 @@
+//ktsh missing positive number
+
+class Solution {
+  public:
+    int kthMissing(vector<int>& arr, int k) {
+        int left = 0, right = arr.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int missing = arr[mid] - (mid + 1);
+
+            if (missing < k) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left + k;
+    }
+};
